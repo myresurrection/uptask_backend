7f507effeba5e5369a6e2a0be81e938b7d1a3f50
myreusrrection <myresurrection@gmail.com>
2025-05-07 14:03:39 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors';
+import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
 import projectRoutes from './routes/projectRoutes'
 
@@ -9,7 +11,7 @@ dotenv.config()
 connectDB();
 
 const app = express();
-
+app.use(cors(corsConfig));
 // Habilitar lectura del body
 
 app.use(express.json());
