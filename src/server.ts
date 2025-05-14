ea5b0221f1ea94e0aee72af322139cfa3ee1801b
myreusrrection <myresurrection@gmail.com>
2025-05-14 17:43:41 -0300

    commit 1

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors';
+import morgan from 'morgan';
 import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
 import projectRoutes from './routes/projectRoutes'
@@ -13,6 +14,9 @@ connectDB();
 const app = express();
 app.use(cors(corsConfig));
 // Habilitar lectura del body
+
+// Loggin de peticiones 
+app.use(morgan('dev'))
 
 app.use(express.json());
 
