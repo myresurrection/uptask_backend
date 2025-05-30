c9861ab542001dffffa68a75be3cbe556169610b
myreusrrection <myresurrection@gmail.com>
2025-05-30 13:26:35 -0300

    update

--- src/middleware/project.ts
+++ src/middleware/project.ts
@@ -10,7 +10,6 @@ declare global {
 }
 
 export async function projectExists(req: Request, res: Response, next: NextFunction) {
-
     try {
         const { projectId } = req.params;
         const project = await Project.findById(projectId);
